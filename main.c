b15207c869f60dda5466d5511bd14ab17007e352
Bernardo Lorenzini <87033537+bernardolorenzini@users.noreply.github.com>
2023-07-19 22:49:48 -0300

    Iniciando o projeto com main.c e com .h
    utilizando variáveis globais para criar o jogo. iniciando o jogo

---
+++ main.c
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  Sprites
+//
+//  Created by Bernardo Lorenzini on 20/06/23.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <glad/glad.h>
+#define SDL_MAIN_HANDLED
+#include <SDL2/SDL.h>
+
+#include"var_globais.h"
+
+
+
+int main(int argc, const char * argv[]) {
+    
+    global.i = 7;
+
+    s_teste.width = 900;
+    s_teste.height = 600;
+
+    return 0;
+}
